8f9a15d7c1475513e2deec7d0d3a7a436ea05dda
thinkfulcommunity <srobinson@thinkful.com>
2014-12-25 06:20:55 +0000

    added new idea function, sort, and upvote saving to db

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,9 +16,17 @@ angular.module('voteful', ['ngMaterial'])
   
   $scope.up = function(project){
     project.upvotes++;
+    console.log(project);
     function success(data) {console.log('success');}
     function error(){console.log('error');}
-    $http.put('/projects/'+project.id, {project: project}).success(success).error(error);
+    $http.put('/projects/'+ project.id, {project: project}).success(success).error(error);
+  };
+  $scope.newIdea = function ($event){
+    $mdDialog.show({
+      targetEvent: $event,
+      templateUrl: 'new-idea.html',
+      controller: 'AddProjectController'
+    });
   };
   
   $scope.down = function(project){
@@ -37,6 +45,18 @@ angular.module('voteful', ['ngMaterial'])
    });
   };
   
+}])
+
+.controller('AddProjectController', ['$scope', '$http', '$mdToast', '$mdDialog', function($scope, $http, $mdToast, $mdDialog, $mdBottomSheet){
+  $scope.sendIdea = function(project, description){
+    function success(data) {console.log('success');}
+    function error(){console.log('error');}
+    $http.post('/projects', {name:project, description:description}).success(success).error(error);
+    $mdDialog.hide();
+  };
+  $scope.close = function(){
+    $mdDialog.hide();
+  };
 }]);
   // $scope.report = function(type, message, user){
   //   function success(data){
